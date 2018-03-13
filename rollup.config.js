195d3ba5b61b495c1322408c11dacd125d3ff628
Sean Olson <nomdegoo@gmail.com>
2018-03-12 20:45:43 -0700

    updated babel preset

--- rollup.config.js
+++ rollup.config.js
@@ -19,7 +19,14 @@ export default {
       commonjs(),
       babel({
         exclude: 'node_modules/**',
-      }),
+        babelrc: false,
+        presets: [
+            ["env", { "modules": false }]
+        ],
+        plugins: [
+            'external-helpers'
+        ],
+    }),
       replace({
         ENV: JSON.stringify(process.env.NODE_ENV || 'development')
       })    
